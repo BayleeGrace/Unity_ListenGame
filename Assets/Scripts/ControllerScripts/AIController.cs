59999bd5bf0aae2e8e9980a648ee7cfeaa7b305f
Baylee Balsimo Ketelhut <baybalsi@uat.edu>
2024-04-18 17:47:09 -0700

    Patrol Waypoints for AI Controller
    In working order! The AI Controller moves to the nearest waypoint, then to the next waypoint in the sequence (and so on). AI State Patrol is working.

--- Assets/Scripts/ControllerScripts/AIController.cs
+++ Assets/Scripts/ControllerScripts/AIController.cs
@@ -5,39 +5,65 @@ using UnityEngine;
 public class AIController : Controller
 {
     public enum AIState { Idle, Patrol, Bloodlust, Whisper, Chase, Silent, Flee, Hide };
+    [Header("Miscellaneous Variables")]
     public AIState currentState;
     private float lastStateChangeTime;
-    //public List<Transform> patrolWaypoints;
+    private GameObject targetPlayer;
+    public float fleeDistance;
+    [Header("Waypoint Variables")]
+    public List<Transform> patrolWaypoints;
+    public Transform nearestWaypoint;
+    public float waypointStopDistance;
+    public bool firstWaypointWasReached = false;
     [Header("Senses Variables")]
     public float hearingDistance;
     public float fieldOfView; // Variable to hold the AI's field of view value
     public float maxViewDistance; // Variable to determine how far the enemy can see
     public GameObject lastHit; // Tracks what the AI is seeing in-game
     public Vector3 collision = Vector3.zero;
+
     public override void Start()
     {
-        
+        targetPlayer = pawn.targetPlayer;
+        ChangeState(AIState.Patrol);
+        currentState = AIState.Patrol;
+    }
+
+    public override void Update()
+    {
+        base.Update();
     }
 
     // Update is called once per frame
-    public override void Update()
+    public void FixedUpdate()
     {
         ProcessInputs();
         CheckRaycast();
-        base.Update();
     }
 
     public override void ProcessInputs()
     {
-        if(IsHasTarget())
+        switch(currentState)
         {
-            switch(currentState)
-            {
-                case AIState.Patrol:
-                    // Do Patrol state
-                    // check for transitions
-                break;
-            }
+            case AIState.Patrol:
+                // Do Patrol state
+                // check for transitions
+                DoPatrolState();
+                if (pawn.IsDistanceLessThan(targetPlayer, fleeDistance))
+                {
+                    ChangeState(AIState.Flee);
+                }
+            break;
+
+            case AIState.Flee:
+                // Do Patrol state
+                // check for transitions
+                DoFleeState();
+                if (!pawn.IsDistanceLessThan(targetPlayer, fleeDistance))
+                {
+                    ChangeState(AIState.Patrol);
+                }
+            break;
         }
     }
 
@@ -45,6 +71,104 @@ public class AIController : Controller
     {
         currentState = newState;
         lastStateChangeTime = Time.time; // Tracks how long it takes to change states when ChangeState is called
+    }
+
+    public void DoIdleState()
+    {
+
+    }
+
+    public void DoPatrolState()
+    {
+        currentState = AIState.Patrol;
+        if (patrolWaypoints != null)
+        {
+            FindNearestPatrolWaypoint();
+            Seek(nearestWaypoint.position);
+            if (pawn.IsDistanceLessThan(nearestWaypoint.gameObject, waypointStopDistance))
+            {
+                firstWaypointWasReached = true;
+                Transform nextWaypoint = nearestWaypoint.GetComponent<PatrolWaypoint>().nextWaypoint.transform;
+                nearestWaypoint = nextWaypoint;
+            }
+        }
+        else
+        {
+            ChangeState(AIState.Idle);
+        }
+    }
+
+    // Create a function to track what PatrolWaypoint is the closest to the current AIController
+    public void FindNearestPatrolWaypoint()
+    {
+        if (firstWaypointWasReached == false)
+        {
+            // Assume the first waypoint in the array is the nearest
+            nearestWaypoint = patrolWaypoints[0].transform;
+
+            float distanceToNearestWaypoint = Vector3.Distance(nearestWaypoint.position, pawn.transform.position);
+
+            // Compare the current waypoint's Vector distance to all other waypoint distances in the list
+            foreach (var patrolWaypoint in patrolWaypoints)
+            {
+                if (patrolWaypoint.gameObject != nearestWaypoint.gameObject)
+                {
+                    // Find the distance between the AI and each patrolWaypoint
+                    float distanceToNewWaypoint = Vector3.Distance(patrolWaypoint.position, pawn.transform.position);
+                    if (distanceToNewWaypoint < distanceToNearestWaypoint)
+                    {
+                        //distanceToNewWaypoint = distanceToNearestWaypoint;
+                        nearestWaypoint = patrolWaypoint.transform;
+                    }
+                }
+            }
+        }
+    }    
+
+    protected void RestartPatrol()
+    {
+        // Reset the current array index back to 0
+        FindNearestPatrolWaypoint();
+    }
+
+    public void DoChaseState()
+    {
+        currentState = AIState.Chase;
+        // Seek our targetPlayer
+        if (targetPlayer != null)
+        {
+        Seek(targetPlayer.transform.position);
+        }
+    }
+
+    public void Seek(Vector3 targetPosition)
+    {
+        // Chase a GAMEOBJECT's position
+        if (targetPlayer != null)
+        {
+            // Rotate towards the targetPlayer
+            pawn.RotateTowards(targetPosition);
+            // Move Forward towards the targetPlayer
+            pawn.MoveForward();
+        }
+        else
+        {
+            //Seek(nearestWaypoint);
+            ChangeState(AIState.Patrol);
+        }
+    }
+
+    public void DoFleeState()
+    {
+        currentState = AIState.Flee;
+        // Find the Vector to our target
+        Vector3 vectorToTarget = targetPlayer.transform.position - pawn.transform.position;
+        // Find the Vector away from our target by multiplying by -1
+        Vector3 vectorAwayFromTarget = -vectorToTarget;
+        // Find the vector we would travel down in order to flee
+        Vector3 fleeVector = vectorAwayFromTarget.normalized * fleeDistance;
+        // Seek the point that is "fleeVector" away from our current position
+        Seek(pawn.transform.position + fleeVector);
     }
 
     public void TargetPlayerOne()
